c5dd4341bfc1195cee6de668b8053b885f5add11
alexdent42 <manhappen@gmail.com>
2022-04-18 17:15:39 +0300

    done

---
+++ script.js
@@ -0,0 +1,55 @@
+var animation = anime.timeline({
+    autoplay: false
+});
+
+animation
+.add({
+    targets:'#btn',
+    top: '1500px',
+    duration: 500, 
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets:'#stars',
+    top: '0px',
+    duration: 1000, 
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets:'#mountains_behind',
+    bottom: '0px',
+    duration: 1000, 
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets:'#moon',
+    top: '0px',
+    duration: 1000, 
+    easing: 'easeInOutBack'
+})
+
+.add({
+    targets:'#mountains_front',
+    bottom: '0px',
+    duration: 1000, 
+    easing: 'easeInOutSine'
+}) 
+
+.add({
+    targets:'section h2',
+   marginRight: '0px',
+    duration: 1000, 
+    easing: 'easeInOutSine'
+})
+
+.add({
+    targets:'#btn',
+    top: '50%',
+    duration: 1000, 
+    easing: 'easeInOutBack'
+})
+
+document.querySelector('#btn').addEventListener('click', animation.play);
